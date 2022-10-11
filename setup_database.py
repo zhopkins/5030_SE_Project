f71366ba34e5f33e22c205a32df80d6e6c790967
Zackary Hopkins <zackary.hopkins@slu.edu>
2022-10-11 11:33:35 -0500

    PE-2 check_user_login and get_product_dic
    Got the API to query the user db and product db

--- setup_database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import database_api
-
-if __name__ == "__main__":
-    username = input("Username: ")
-    password = input("Password: ")
-    url = "mongodb+srv://" + username + ":" + password + "@fecomdata.3k9eben.mongodb.net/?retryWrites=true&w=majority"
-    
-    mgClient = MongoClient(url)
