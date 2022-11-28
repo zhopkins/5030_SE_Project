d873c27bd2757d133eacc639796038eb37bbf651
Zackary Hopkins <zackary.hopkins@slu.edu>
2022-11-28 10:56:49 -0600

    PE-20 reorganizing files

--- src/app.py
+++ src/app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
-
+from database_api import database_api
 
 app = Flask(__name__)
 
+API = database_api('admin', 'K6hzJEOyNUGI6rOW')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/', methods=['POST'])
+def login_attempt():
+    user_name = request.form['login_txt']
+    password = request.form['password_txt']
+
+    if API.check_user_login(user_name, password) != None:
+        return render_template('index.html')
+    else: 
+        print(user_name)
+        print(password)
+        return render_template('login.html')
 
 
 if __name__ == "__main__":
